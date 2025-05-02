e175e44e91009e44e1b39a416c93c95cc61a2331
nao-United92 <naoyauver@icloud.com>
2025-05-03 00:43:18 +0900

    mapでブログデータの取り出し

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import { getDocs, collection } from 'firebase/firestore/lite';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase'; // Adjust the import based on your project structure
 import './Home.css'; // Assuming you have a CSS file for styling
 
 const Home = () => {
+  const [postLists, setPostLists] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">今はReactの学習中です。</div>
-        <div className="nameAndDeleteButton">
-          <h3>@</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postLists.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
