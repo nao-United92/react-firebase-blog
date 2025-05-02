d5cfd9886bef6fe55acca6b5ef8285f37bfeb39a
nao-United92 <naoyauver@icloud.com>
2025-05-02 23:50:19 +0900

    title・postTextの内容取得

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreatePost.css'; // Assuming you have a CSS file for styling
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
+  const createPost = async () => {};
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入" />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder="投稿内容を記入"></textarea>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postButton">投稿する</button>
+        <button className="postButton" onClick={createPost}>
+          投稿する
+        </button>
       </div>
     </div>
   );
