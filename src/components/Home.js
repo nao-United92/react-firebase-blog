6b342a5c39b091744eb568657d3f9a27e8df90ba
nao-United92 <naoyauver@icloud.com>
2025-05-03 00:10:03 +0900

    Homeコンポーネントの作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">今はReactの学習中です。</div>
+      <div className="nameAndDeleteButton">
+        <h3>@</h3>
+        <button>削除</button>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
