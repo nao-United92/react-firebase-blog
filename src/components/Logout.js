505c3ebc42118a62040be2efb16f58b34495b3a3
nao-United92 <naoyauver@icloud.com>
2025-05-02 14:57:05 +0900

    googleログアウト(signOut)の実装

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { signInWithPopup, signOut } from 'firebase/auth';
+import React from 'react';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate;
+  const logout = () => {
+    // ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/login');
+    });
+  };
+
   return (
-    <div>Logout</div>
-  )
-}
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>Googleでログアウト</button>
+    </div>
+  );
+};
 
-export default Logout
+export default Logout;
